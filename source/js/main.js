dc0ac9f144943df3cb7b8b12c3a2e5f0809c11a7
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-07-21 23:28:41 +0300

    чутка процесса

---
+++ source/js/main.js
@@ -0,0 +1,18 @@
+var navMain = document.querySelector('.nav');
+var navToggle = document.querySelector('.nav__toggle');
+var headerClosed = document.querySelector('.page-header');
+
+navMain.classList.remove('nav--nojs');
+headerClosed.classList.add('page-header--closed');
+
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('nav--closed')) {
+        navMain.classList.remove('nav--closed');
+        headerClosed.classList.remove('page-header--closed');
+        navMain.classList.add('nav--opened');
+    } else {
+        navMain.classList.add('nav--closed');
+        headerClosed.classList.add('page-header--closed');
+        navMain.classList.remove('nav--opened');
+    }
+});
