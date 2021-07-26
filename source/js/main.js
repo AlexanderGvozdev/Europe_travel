3c46927d295227c5c6b94e2857efba014290cdf7
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-07-27 02:15:56 +0300

    открытие попала + правки промежуточной сетки

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,10 @@
-var navMain = document.querySelector('.nav');
-var navToggle = document.querySelector('.nav__toggle');
-var headerClosed = document.querySelector('.page-header');
+const navMain = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+const headerClosed = document.querySelector('.page-header');
+const buttonOpen = document.querySelector('.countries-desc__item-wrapper-button');
+const buttonClose = document.querySelector('.popup__wrapper-button-close');
+const popup = document.querySelector('.popup');
+const inputTel = document.querySelector('.popup__wrapper-tel');
 
 navMain.classList.remove('nav--nojs');
 headerClosed.classList.add('page-header--closed');
@@ -15,4 +19,24 @@ navToggle.addEventListener('click', function() {
         headerClosed.classList.add('page-header--closed');
         navMain.classList.remove('nav--opened');
     }
+});
+
+buttonOpen.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('popup-show');
+  inputTel.focus();
+});
+
+buttonClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('popup-show')
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains('popup-show')) {
+      evt.preventDefault();
+      popup.classList.remove('popup-show');
+    }
+  }
 });
