e13952cbb67873415e04236201c207887c9eb5cc
AlexanderGvozdev <aleksandrgvozdev1@gmail.com>
2021-07-27 14:38:15 +0300

    по мелочам

--- source/js/main.js
+++ source/js/main.js
@@ -1,22 +1,18 @@
 const navMain = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
-const headerClosed = document.querySelector('.page-header');
 const buttonOpen = document.querySelector('.countries-desc__item-wrapper-button');
 const buttonClose = document.querySelector('.popup__wrapper-button-close');
 const popup = document.querySelector('.popup');
 const inputTel = document.querySelector('.popup__wrapper-tel');
 
 navMain.classList.remove('nav--nojs');
-headerClosed.classList.add('page-header--closed');
 
 navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('nav--closed')) {
         navMain.classList.remove('nav--closed');
-        headerClosed.classList.remove('page-header--closed');
         navMain.classList.add('nav--opened');
     } else {
         navMain.classList.add('nav--closed');
-        headerClosed.classList.add('page-header--closed');
         navMain.classList.remove('nav--opened');
     }
 });
